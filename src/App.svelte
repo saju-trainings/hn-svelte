b8a8a88320431900e265ea440c5b1c0837ecd9b0
Saju Thankappan <sajuthankappan@gmail.com>
2022-11-04 11:09:09 +0000

    added basic navigation

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,20 @@
 <script>
   import Header from "./lib/Header.svelte";
-    import New from "./pages/New.svelte";
+  import New from "./pages/New.svelte";
+    import Past from "./pages/Past.svelte";
+  
+  let page = 'new';
 </script>
 
 <main>
   <h1>Hacker News - Svelte</h1>
-  <Header />
-  <New />
+  <Header bind:page={page}/>
+  
+  {#if page === 'new'}
+    <New />
+  {:else if page === 'past'}
+    <Past />
+  {/if}
 </main>
 
 <style>
--- src/lib/Header.svelte
+++ src/lib/Header.svelte
@@ -1,12 +1,23 @@
-<div>
-	<span>new</span>
-	<span>past</span>
-	<span>comments</span>
-	<span>ask</span>
-</div>
+<script>
+	export let page = 'new';
+	
+</script>
+
+<nav>
+	<span on:click={() => page = 'new'}>new</span>
+	<span on:click={() => page = 'past'}>past</span>
+	<span on:click={() => page = 'comments'}>comments</span>
+	<span on:click={() => page = 'ask'}>ask</span>
+</nav>
 
 <style>
+	nav {
+		background-color: lightblue;
+		padding: .5rem;
+	}
+	
 	span {
 		padding: 0.5rem;
+		cursor: pointer;
 	}
 </style>
