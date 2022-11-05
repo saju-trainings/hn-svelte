171e3afd474e10729c7ad09205d06b5ba3719408
Saju Thankappan <sajuthankappan@gmail.com>
2022-11-05 11:35:47 +0000

    initial setup for user list

---
+++ src/pages/Users.svelte
@@ -0,0 +1,34 @@
+<svelte:head>
+  <title>Hacker News - Svelte - New</title>
+</svelte:head>
+
+<script>
+	import { onMount } from "svelte";
+	import NewsItemSummary from "../lib/NewsItemSummary.svelte";
+	let items = [];
+
+	onMount(async () => {
+		const url = "https://api.hackerwebapp.com/news";
+		const response = await fetch(url);
+
+		if (response.ok) {
+			items = await response.json();
+		} else {
+			alert("HTTP-Error: " + response.status);
+		}
+	});
+</script>
+
+<h2>Users</h2>
+
+{#each items as item}
+  <div class="news-item-container">
+		<NewsItemSummary {item}/>
+	</div>
+{/each}
+
+<style>
+	.news-item-container {
+		padding: .5rem;
+	}
+</style>
