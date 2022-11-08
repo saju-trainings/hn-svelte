217a76ec9ba46827344f5db145afafac67f5e4e2
Saju Thankappan <sajuthankappan@gmail.com>
2022-11-08 11:16:06 +0000

    Use flex grid for users list

--- src/pages/Users.svelte
+++ src/pages/Users.svelte
@@ -23,15 +23,26 @@
 <div class="container is-max-desktop">
 	<h2 class="is-size-2">Users</h2>
 
-	{#each users as user}
-		<div class="user-item-container">
-			<UserSummary {user} />
-		</div>
-	{/each}
+	<div class="columns is-tablet is-flex-wrap-wrap">
+		{#each users as user}
+			<div class="user-item-container">
+				<UserSummary {user} />
+			</div>
+		{/each}
+	</div>
 </div>
 
 <style>
 	.user-item-container {
 		padding: 0.5rem;
+		width: 20rem;
+		max-width: 20rem;
 	}
+	
+	/*@media screen and (min-width: 660px) {
+		.users-container {
+			display: flex;
+			flex-wrap: wrap;
+		}
+	}*/
 </style>
